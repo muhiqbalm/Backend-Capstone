b5b1c2a350f9261b7872b351466d6c3489692c91
Muhammad Iqbal Masykuri <muhammadiqbal@synapsis.id>
2023-11-01 13:20:10 +0700

    predict model

--- app.py
+++ app.py
@@ -1,40 +1,28 @@
-from flask import Flask, request,jsonify
-from flask_socketio import SocketIO,emit
-from flask_cors import CORS
+from flask import Flask, request, jsonify
+import pandas as pd
+import joblib
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-CORS(app,resources={r"/*":{"origins":"*"}})
-socketio = SocketIO(app,cors_allowed_origins="*")
 
-@app.route("/")
-def home():
-    return "Hello, Flask!"
+# Load the Random Forest model
+model = joblib.load("rf_classifier_model.h5")
 
-@app.route("/http-call")
-def http_call():
-    """return JSON with string data as the value"""
-    data = {'data':'This text was fetched using an HTTP call to server on render'}
-    return jsonify(data)
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        # Get the JSON input data
+        data = request.get_json()
+        # Create a DataFrame from the input data
+        df = pd.DataFrame(data)
 
-@socketio.on("connect")
-def connected():
-    """event listener when client connects to the server"""
-    print(request.sid)
-    print("client has connected")
-    emit("connect",{"data":f"id: {request.sid} is connected"})
+        # Make predictions using the loaded model
+        predictions = model.predict(df)
 
-@socketio.on('data')
-def handle_message(data):
-    """event listener when client types a message"""
-    print("data from the front end: ",str(data))
-    emit("data",{'data':data,'id':request.sid},broadcast=True)
+        # Return the predictions as JSON
+        return jsonify({"predictions": predictions.tolist()})
 
-@socketio.on("disconnect")
-def disconnected():
-    """event listener when client disconnects to the server"""
-    print("user disconnected")
-    emit("disconnect",f"user {request.sid} disconnected",broadcast=True)
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True,port=5001)
+    app.run(debug=True)
